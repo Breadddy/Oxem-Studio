d9e6f42ca6d8f4098f3ad8384b653cbe04422e1d
Breadddy <72420022+Breadddy@users.noreply.github.com>
2020-12-03 00:04:08 +0300

    Update main.php

--- Oxem/main.php
+++ Oxem/main.php
@@ -1,8 +1,9 @@
 <?php
 require_once('class.php');
 $MyBarn = new barn;
-$MyBarn->getProduce();    //подоить коров и собрать яйца у куриц
-$MyBarn->countProduce();  //посчитать произведённую продукцию
-echo 'Курицы снесли '.$MyBarn->getAllEggs().' шт. яиц'.PHP_EOL;
-echo 'Коровы дали '.$MyBarn->getAllMilk().' л. молока'.PHP_EOL;
+$MyBarn->addAnimals(20, 'chicken'); //добавить новых животных в амбар
+$MyBarn->addAnimals(10, 'cow');
+$MyBarn->getProduce();    //получить продукцию со всех животных
+echo 'Курицы снесли '.$MyBarn->countAnimalProduce('chicken').' шт. яиц'.PHP_EOL;
+echo 'Коровы дали '.$MyBarn->countAnimalProduce('cow').' л. молока'.PHP_EOL;
 
